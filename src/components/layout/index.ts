a3ead70d815523bc626f46daa00c21d23f4627a6
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-24 18:18:13 +0400

    fix: export layout component barrel

---
+++ src/components/layout/index.ts
@@ -0,0 +1,5 @@
+export { EditorialGrid } from "./editorial-grid";
+export { PageShell } from "./page-shell";
+export { ReadingColumn } from "./reading-column";
+export { Section } from "./section";
+export { SplitLayout } from "./split-layout";
