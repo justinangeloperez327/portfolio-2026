c0574613e1718b08036666adc13ef2ec354547da
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-24 17:20:14 +0400

    refactor: establish content architecture

---
+++ src/content/index.ts
@@ -0,0 +1,13 @@
+export { aboutFocusAreas } from "./about";
+export { contactConversationAreas, contactMethods } from "./contact";
+export {
+  engineeringArchitectureLayers,
+  engineeringPrinciples,
+  engineeringRequestFlow,
+} from "./engineering";
+export { homeContent } from "./home";
+export {
+  resumeCapabilities,
+  resumeCurrentWork,
+  resumeTechnologies,
+} from "./resume";
