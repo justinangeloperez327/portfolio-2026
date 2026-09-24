ec46b3b62049d6449ca2e3e53f3abb4fc58933d7
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-24 16:11:43 +0400

    feat: add typography components

---
+++ src/components/typography/index.ts
@@ -0,0 +1,7 @@
+export { DisplayHeading } from "./display-heading";
+export { EditorialAccent } from "./editorial-accent";
+export { Eyebrow } from "./eyebrow";
+export { Lede } from "./lede";
+export { Prose } from "./prose";
+export { SectionHeading } from "./section-heading";
+export { TechnicalLabel } from "./technical-label";
