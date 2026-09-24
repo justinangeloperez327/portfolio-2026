b6f3d677085018a62a122b4a6daacb76884adf7d
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-24 16:26:28 +0400

    feat: add case study system

---
+++ src/components/case-study/index.ts
@@ -0,0 +1,4 @@
+export { CaseStudyHeader } from "./case-study-header";
+export { CaseStudyLayout } from "./case-study-layout";
+export { CaseStudyNav } from "./case-study-nav";
+export { CaseStudySection } from "./case-study-section";
