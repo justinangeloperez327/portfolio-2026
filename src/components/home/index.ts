4ef2ee369042f1a1868a7f5adf07a7d237010fc3
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-24 16:18:16 +0400

    feat: export homepage content sections

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,1 +1,5 @@
+export { ContactCallout } from "./contact-callout";
+export { EngineeringPrinciples } from "./engineering-principles";
 export { Hero } from "./hero";
+export { ProfileSummary } from "./profile-summary";
+export { SelectedWork } from "./selected-work";
