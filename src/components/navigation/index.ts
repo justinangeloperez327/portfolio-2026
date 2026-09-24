d831b294845d88f49b088950c85f5a679013a6b5
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-24 16:15:22 +0400

    feat: add global navigation system

---
+++ src/components/navigation/index.ts
@@ -0,0 +1,4 @@
+export { MobileNavigation } from "./mobile-navigation";
+export { PrimaryNavigation } from "./primary-navigation";
+export { SiteFooter } from "./site-footer";
+export { SiteHeader } from "./site-header";
