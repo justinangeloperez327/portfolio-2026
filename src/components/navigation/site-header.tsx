e7466387fe6cb676ad14c1faee5fad5886a65c1d
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-24 19:59:01 +0400

    design: refine global portfolio header

--- src/components/navigation/site-header.tsx
+++ src/components/navigation/site-header.tsx
@@ -1,14 +1,12 @@
 import Link from "next/link";
-import { siteConfig } from "@/config/site";
 import { PrimaryNavigation } from "./primary-navigation";
 
 export function SiteHeader() {
   return (
     <header className="site-header">
-      <div className="container-page site-header__inner">
+      <div className="site-header__inner">
         <Link className="site-brand" href="/" aria-label="Justin Angelo Perez, home">
-          <span>JAP</span>
-          <span className="site-brand__role">{siteConfig.title}</span>
+          <span>JAP</span><span className="site-brand__role">Justin Angelo Perez</span>
         </Link>
         <PrimaryNavigation />
       </div>
